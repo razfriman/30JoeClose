3ccb7f0b6ce692fa5b51a39ec90daec3f65e90a2
Raz Friman <raz@canva.com>
2022-12-29 09:28:28 +1100

    Clean

--- web/main.js
+++ web/main.js
@@ -1,23 +1,79 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function haltEvent(event) {
+  event.preventDefault && event.preventDefault();
+  event.stopPropagation && event.stopPropagation();
+  event.cancelBubble = true;
+  event.returnValue = false;
+  return false;
+}
 
-setupCounter(document.querySelector('#counter'))
+// prevent right-click menus from appearing
+document.addEventListener("contextmenu", haltEvent);
+// more attempts to prevent right-click menus from appearing -- avoid; they block touchscreen/iPad controls
+// document.addEventListener("touchmove", haltEvent );
+// document.addEventListener("touchcancel", haltEvent );
+
+// add events for both touch and mouse controls
+function addButtonEventListeners(buttonElementID, startFunction, endFunction) {
+  let element = document.getElementById(buttonElementID);
+  element.addEventListener("touchstart", startFunction);
+  element.addEventListener("mousedown", startFunction);
+  element.addEventListener("touchend", endFunction);
+  element.addEventListener("mouseup", endFunction);
+}
+
+function associateMovementControls(buttonElementID, component, keyName) {
+  addButtonEventListeners(
+    buttonElementID,
+    function (event) {
+      component.registerKeyDown(keyName);
+      return haltEvent(event);
+    },
+    function (event) {
+      component.registerKeyUp(keyName);
+      return haltEvent(event);
+    }
+  );
+}
+
+// need to run javascript code after a-scene entities and components are loaded
+AFRAME.registerComponent("screen-controls", {
+  init: function () {
+    let component =
+      document.getElementById("camera").components["extended-wasd-controls"];
+
+    associateMovementControls(
+      "buttonMoveForward",
+      component,
+      component.data.moveForwardKey
+    );
+    associateMovementControls(
+      "buttonMoveBackward",
+      component,
+      component.data.moveBackwardKey
+    );
+    associateMovementControls(
+      "buttonMoveLeft",
+      component,
+      component.data.moveLeftKey
+    );
+    associateMovementControls(
+      "buttonMoveRight",
+      component,
+      component.data.moveRightKey
+    );
+    associateMovementControls(
+      "buttonMoveUp",
+      component,
+      component.data.moveUpKey
+    );
+    associateMovementControls(
+      "buttonMoveDown",
+      component,
+      component.data.moveDownKey
+    );
+  },
+
+  tick: function (time, deltaTime) {},
+});
