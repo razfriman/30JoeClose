b9b1c24c9a21919b1cbc426376174a89b1d95231
Raz Friman <raz@canva.com>
2023-01-09 14:58:53 +1100

    Save

--- web/main.js
+++ web/main.js
@@ -10,3 +10,13 @@ function haltEvent(event) {
 document.addEventListener("contextmenu", haltEvent);
 document.addEventListener("touchmove", haltEvent);
 document.addEventListener("touchcancel", haltEvent);
+
+if (/Android|iPhone/i.test(navigator.userAgent)) {
+  for (const e of document.getElementsByClassName("desktop")) {
+    e.remove();
+  }
+} else {
+  for (const e of document.getElementsByClassName("mobile")) {
+    e.remove();
+  }
+}
